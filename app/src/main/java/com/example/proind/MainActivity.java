f58aa224ef2377c347fecd1e43f3da4763d8c20c
Luis Murillo <luisales.999@gmail.com>
2019-07-31 22:10:04 -0600

    Boton next, back y audio

--- app/src/main/java/com/example/proind/MainActivity.java
+++ app/src/main/java/com/example/proind/MainActivity.java
@@ -1,60 +1,67 @@
 package com.example.proind;
 
+import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.annotation.NonNull;
 
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mTextMessage;
+    Button bb, bn;
+    ImageView iv1;
+    Integer cont =1 ;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceSate) {
+        super.onCreate(savedInstanceSate);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        bn = (Button) findViewById(R.id.btn_next);
+        bb = (Button) findViewById(R.id.btn_back);
+        iv1 = (ImageView) findViewById(R.id.imageView2);
 
-        final MediaPlayer negro = MediaPlayer.create(this,R.raw.negro);
-        Button button = (Button) this.findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
+        bn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0){
+                if (cont==1  )
+                    iv1.setImageResource(R.drawable.color2);
+                if (cont==2  )
+                    iv1.setImageResource(R.drawable.color3);
+                cont++;
+            }
+        });
+        bb.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View arg0){
+                if (cont==1 || cont==2 )
+                    iv1.setImageResource(R.drawable.color1);
+                if (cont==3 )
+                    iv1.setImageResource(R.drawable.color2);
+                cont --;
+            }
+        });
+        final MediaPlayer audio = MediaPlayer.create(this,R.raw.sonido1);
+        Button btnaudio = (Button) this.findViewById(R.id.btn_audio);
+        btnaudio.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                 negro.start();
-                negro.start();
-                negro.start();
+                audio.start();
+
 
             }
         });
     }
+
+
+
+
+
+
 
 }
